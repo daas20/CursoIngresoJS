be3765a8ba6976fe483e8bea560c060321deae0a
daas20 <60674736+daas20@users.noreply.github.com>
2020-02-05 22:27:28 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,17 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad; // declaro la variable
+
+edad = parseInt (document.getElementById("edad").value);  //tomo el valor ingresado en el cuadro de texto 
+
+if(edad == 15) /*operardor "igual". Tambien puedo poner
+                if(edad === 15) porque el triple "igual" compara tambien por tipo*/
+{
+   alert("niña bonita");
+   // si la edad es igual a 15 entonces se cumple la condicion y muestro la frase "niña bonita"
+
+}
 
 
 }//FIN DE LA FUNCIÓN
