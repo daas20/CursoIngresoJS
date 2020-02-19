f295f9b40549c2092cefeb68bf0be9829f82b2aa
daas20 <60674736+daas20@users.noreply.github.com>
2020-02-19 16:21:48 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,28 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+var mes = document.getElementById('mes').value;  //le sacamos "delAño" a "mesDelAño" porque usa caracter latino
 
-
-
+switch (mes)
+{
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+        alert ("Falta para el invierno");
+        break;  
+    case "Julio":
+    case "Agosto":
+        alert ("Abrigate que hace frio");
+        break;
+    default:    
+    alert ("Ya pasamos el frio, ahora calor!!!"); 
+}
+/* al tener la misma linea de codigo los "case" de Enero a Junio,
+saco la line de codigo y el break y solo lo pongo en el ultimo "case" de los que tienen el mismo codigo
+asi ahorro lineas de codigo y no tengo que escribir lo mismo en varios "case"*/
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;  //le sacamos "delAño" a "mesDelAño" porque usa caracter latino
 
-//alert (mesDelAño);
-	
-	
-
-
+switch (mes)
+{
+    case "Febrero":
+        alert ("Tiene 28 días");
+        break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":   
+        alert ("Tiene 30 dìas");
+        break;
+    default:    
+        alert ("Tiene 31 días"); 
+}
 
 }//FIN DE LA FUNCIÓN
